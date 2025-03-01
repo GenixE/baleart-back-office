e82eb3444510947c7c040eb72420d118608085f8
Gian Carlo Valencia <106990876+GenixE@users.noreply.github.com>
2025-03-01 04:12:02 +0100

    Added a button to redirect to /spaces

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,12 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <div class="mt-4">
+                        <a href="/dashboard/spaces"
+                           class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
+                            Spaces
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
